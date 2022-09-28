75dec6bced20fcdf3e7bf4efea1e0faf7667877c
wingstako <wingstako@proton.me>
2022-09-28 17:06:42 +0800

    feat: 💬 update "about"

--- src/lib/tabs/about.svelte
+++ src/lib/tabs/about.svelte
@@ -1,5 +1,36 @@
-<div>
-    <h1>
-        Hi you should stop nutting
-    </h1>
+<script lang="ts">
+</script>
+
+<!-- Example #4 - external ♥-icon -->
+<link
+  rel="stylesheet"
+  type="text/css"
+  href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+/>
+
+<div class="flex-container">
+  <img alt="horny samurai" src="/images/horny_samurai.jpeg" />
+
+  <blockquote>
+    <p><i>I do not wish to be horny anymore.</i></p>
+    <p><i>I just want to be happy.</i></p>
+
+    <p class="signature">- Horny Samurai</p>
+  </blockquote>
 </div>
+
+<style scoped>
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    align-items: center;
+    text-align: center;
+    width: 16rem;
+    padding-bottom: 0.5rem;
+  }
+
+  .signature {
+    float: right;
+  }
+</style>
